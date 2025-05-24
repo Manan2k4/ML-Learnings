9d4cfa0f86c50572f0678ced65d293cbe95ea55c
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-24 15:30:41 +0530

    (*_*)

---
+++ pandas_learning/descriptive_statistics.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "one": pd.Series(np.random.randn(3), index = ["a", "b", "c"]),
+        "two": pd.Series(np.random.randn(4), index = ["a", "b", "c", "d"]),
+        "three": pd.Series(np.random.randn(3), index=["b", "c", "d"]),
+    }
+)
+print(df,'\n')
+
+print(df.mean(0),'\n')
+
+print(df.mean(1),'\n')
+
+print(df.sum(0, skipna = False), '\n')
+
+print(df.sum(axis=1, skipna=True), '\n')
+
