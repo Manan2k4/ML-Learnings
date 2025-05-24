82b3ec2f9e263226765d20aabba96c50f435001a
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-24 16:54:06 +0530

    Update summarizing_data.py

--- pandas_learning/summarizing_data.py
+++ pandas_learning/summarizing_data.py
@@ -5,4 +5,8 @@ series = pd.Series(np.random.randn(1000))
 
 series[::2] = np.nan
 
-series.describe()
+frame = pd.DataFrame(np.random.randn(1000,5), columns=["a", "b", "c", "d", "e"])
+
+frame.iloc[::2] = np.nan
+
+print(frame.describe)
