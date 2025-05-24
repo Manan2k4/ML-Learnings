a4a710f0289412a98ddf80d48ff1ec076fa04c43
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-24 11:10:34 +0530

    Update viewing_tables.py

--- pandas_learning/viewing_tables.py
+++ pandas_learning/viewing_tables.py
@@ -13,3 +13,19 @@ print(df.columns)
 
 print(df.to_numpy())
 
+df2 = pd.DataFrame(
+    {
+        "A": 1.0,
+        "B": pd.Timestamp("20130102"),
+        "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+        "D": np.array([3] * 4, dtype="int32"),
+        "E": pd.Categorical(["test", "train", "test", "train"]),
+        "F": "foo",     # Scalar broadcast applied to all rows
+    }
+)
+
+print(df2.dtypes)
+print(df2.to_numpy())
+print(df.describe())
+print(df.T)
+print(df.sort_index(axis=1, ascending = False))
