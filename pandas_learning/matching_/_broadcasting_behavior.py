8be07dfcf651fd412c834a2711e0b58f8a7a7bb5
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-24 14:17:12 +0530

    Update _broadcasting_behavior.py

--- pandas_learning/matching_/_broadcasting_behavior.py
+++ pandas_learning/matching_/_broadcasting_behavior.py
@@ -17,3 +17,15 @@ column = df["two"]
 print(column,"\n")
 
 print(df.sub(row, axis="columns"),'\n')
+print(df.sub(row, axis=1),'\n')
+print(df.sub(column, axis="index"),'\n')
+print(df.sub(column, axis=0),'\n')
+
+dfmi = df.copy()
+dfmi.index = pd.MultiIndex.from_tuples(
+    [(1, "a"), (1, "b"), (1, "c"), (2, "a")], names=["first", "second"]
+)
+
+print(dfmi.sub(column, axis=0, level="second"))
+
+s = pd.Series(np.arange(10))
