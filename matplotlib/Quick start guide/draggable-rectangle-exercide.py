cfbba92550df0ea644fdd5f8424adf7ac65537ad
Manan2k4 <ghoniyamanan@gmail.com>
2025-05-27 15:37:31 +0530

    draggable rect operational

---
+++ matplotlib/Quick start guide/draggable-rectangle-exercide.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class DraggableRectangle:
+    def __init__(self, rect):
+        self.rect = rect
+        self.press = None
+
+    def connect(self):
+        """Connect to all events we need."""
+        self.cidpress = self.rect.figure.canvas.mpl_connect(
+            'button_press_event', self.on_press)
+        self.cidrelease = self.rect.figure.canvas.mpl_connect(
+            'button_release_event', self.on_release)
+        self.cidmotion = self.rect.figure.canvas.mpl_connect(
+            'motion_notify_event', self.on_motion)
+
+        
+    def on_press(self, event):
+        """Check whether mouse is over us; 
+        if so, store some data."""
+        if event.inaxes != self.rect.axes:
+            return
+        contains, attrd = self.rect.contains(event)
+        if not contains:
+            return
+        print('event contains', self.rect.xy)
+        self.press = self.rect.xy, (event.xdata, event.ydata)
+
+    def on_motion(self, event):
+        """Move rectangle if mouse is over us."""
+        if self.press is None or event.inaxes != self.rect.axes:
+            return
+        (x0, y0), (xpress, ypress) = self.press
+        dx = event.xdata - xpress
+        dy = event.ydata - ypress
+
+        self.rect.set_x(x0 + dx)
+        self.rect.set_y(y0 + dy)
+
+        self.rect.figure.canvas.draw()
+
+    def on_release(self, event):
+        """Disconnect all callbacks."""
+        self.rect.figure.canvas.mpl_disconnect(self.cidpress)
+        self.rect.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.rect.figure.canvas.mpl_disconnect(self.cidmotion)
+
+fig, ax = plt.subplots()
+rects = ax.bar(range(10), 20 * np.random.rand(10))
+drs = []
+for rect in rects:
+    dr = DraggableRectangle(rect)
+    dr.connect()
+    drs.append(dr)
+
+plt.show()
